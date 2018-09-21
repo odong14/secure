760657cab00b88f465689ec6531fda15174a17c3
odong14 <muhammad.adli.riz@gmail.com>
2018-09-21 10:59:30 +0700

    file TestAESCTR sudah dapat menggunakan AES CTR tapi belum dipisahkan

--- TestAESCTR.java
+++ TestAESCTR.java
@@ -10,6 +10,8 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;  
 import javax.crypto.spec.IvParameterSpec;  
 import javax.crypto.spec.SecretKeySpec;  
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
 import javax.xml.bind.DatatypeConverter;  
 
 public class TestAESCTR{
@@ -19,7 +21,7 @@ public class TestAESCTR{
     int LongKeyByte = 32;
     int LongNounceAndCounter = 16;
     byte[] plain = new byte[DataNumber];
-    byte[] key = new byte[LongKeyByte];
+    byte[] keyBytes = new byte[LongKeyByte];
     byte[] nonceAndCounter = new byte[LongNounceAndCounter];
     
     //Generate dummy data
@@ -34,11 +36,11 @@ public class TestAESCTR{
 
     //Generate dummy key 
     for(byte i = 0; i < LongKeyByte; i++){
-        key[i] = (byte)(rand.nextInt(256) - 128);
+        keyBytes[i] = (byte)(rand.nextInt(256) - 128);
     }
     System.out.println("key:");
     for(byte i = 0; i < LongKeyByte; i++){
-        System.out.print(key[i] + " ");
+        System.out.print(keyBytes[i] + " ");
     }
     System.out.println("");
 
@@ -52,5 +54,45 @@ public class TestAESCTR{
     }
     System.out.println("");
 
+    SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+    IvParameterSpec ivSpec = new IvParameterSpec(nonceAndCounter);
+    Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+
+    // encryption pass
+    cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+    ByteArrayInputStream bIn = new ByteArrayInputStream(plain);
+    CipherInputStream cIn = new CipherInputStream(bIn, cipher);
+    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+    int ch;
+    while ((ch = cIn.read()) >= 0) {
+      bOut.write(ch);
+    }
+
+    byte[] cipherText = bOut.toByteArray();
+    byte[] encrypted = bOut.toByteArray();
+
+    //Print encrypted data
+    System.out.println("Encrypted data:");
+    for(byte i = 0; i < encrypted.length; i++){
+        System.out.print(encrypted[i] + " ");
+    }
+    System.out.println("");
+
+    // decryption pass
+    cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+    bOut = new ByteArrayOutputStream();
+    CipherOutputStream cOut = new CipherOutputStream(bOut, cipher);
+    cOut.write(cipherText);
+    cOut.close();
+    
+    byte[] decrypted = bOut.toByteArray();
+
+    //Print decrypted data
+    System.out.println("Decrypted data:");
+    for(byte i = 0; i < decrypted.length; i++){
+        System.out.print(decrypted[i] + " ");
+    }
+    System.out.println("");
     }
 }
