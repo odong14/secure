a1ad3c04d3fa91e3a251aafcdcf01dfb38735c37
odong14 <muhammad.adli.riz@gmail.com>
2018-10-26 05:57:12 +0700

    Menggunakan padding

--- TestTwoFish256.java
+++ TestTwoFish256.java
@@ -12,19 +12,20 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;  
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
-//import com.google.protobuf.ByteString; 
-//import javax.xml.bind.DatatypeConverter;  
 
 public class TestTwoFish256{
     public static void main(String [] args) throws Exception {
     Random rand = new Random();
-    int DataNumber = 14;
+    int DataNumber = 1;
     int LongKeyByte = 32;
     int LongNounceAndCounter = 16;
+    int blockSize = 16; 
+    int blockSizePad;
     byte[] plain = new byte[DataNumber];
     byte[] keyBytes = new byte[LongKeyByte];
     byte[] nonceAndCounter = new byte[LongNounceAndCounter];
     Twofish twofish = new Twofish();
+    PKCS7 padding = new PKCS7();
 
     //Generate dummy data
     for(int i = 0; i < DataNumber; i++){
@@ -45,11 +46,18 @@ public class TestTwoFish256{
         System.out.print(keyBytes[i] + " ");
     }
     System.out.println("");
-    Object keyObject = twofish.makeKey(keyBytes, 16);
+    Object keyObject = twofish.makeKey(keyBytes, blockSize);
 
-    byte[] encrypted = new byte[DataNumber];
-    for (int i=0; i<Array.getLength(plain); i+=16) {
-        twofish.encrypt(plain, 0, encrypted, 0, keyObject, 16);
+    //Add padding if need
+    padding.init(blockSize);
+    byte pad[] = padding.pad(plain, 0, plain.length);
+    byte pt[] = new byte[plain.length + pad.length];
+    System.arraycopy(plain, 0, pt, 0, plain.length);
+    System.arraycopy(pad, 0, pt, plain.length, pad.length);
+
+    byte[] encrypted = new byte[pt.length];
+    for (int i=0; i<Array.getLength(pt); i+=blockSize) {
+        twofish.encrypt(pt, 0, encrypted, 0, keyObject, blockSize);
     }
     
     //Print encrypted data
@@ -60,17 +68,20 @@ public class TestTwoFish256{
     System.out.println("");
 
     Object keydecObject = twofish.makeKey(keyBytes, 16);
-    byte[] decrypted = new byte[DataNumber];
+    byte[] decrypted = new byte[encrypted.length];
 
     for (int i=0; i<Array.getLength(encrypted); i+=16) {
         twofish.decrypt(encrypted, i, decrypted, i, keydecObject, 16);
     }
     
-    
+    int unpad = padding.unpad(decrypted, 0, decrypted.length);
+    byte[] output = new byte[decrypted.length - unpad];
+
+    System.arraycopy(decrypted, 0, output, 0, output.length);
     //Print decrypted data
     System.out.println("Decrypted data:");
-    for(int i = 0; i < decrypted.length; i++){
-        System.out.print(decrypted[i] + " ");
+    for(int i = 0; i < output.length; i++){
+        System.out.print(output[i] + " ");
     }
     System.out.println("");
     }
