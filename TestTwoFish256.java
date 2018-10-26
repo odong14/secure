8dc4db3490b5d56e28f6ec0f7623b2ce9b565723
odong14 <muhammad.adli.riz@gmail.com>
2018-10-26 07:00:15 +0700

    Baru bisa buat 16 byte

--- TestTwoFish256.java
+++ TestTwoFish256.java
@@ -16,16 +16,19 @@ import javax.crypto.CipherOutputStream;
 public class TestTwoFish256{
     public static void main(String [] args) throws Exception {
     Random rand = new Random();
-    int DataNumber = 1;
+    int DataNumber = 16;
     int LongKeyByte = 32;
     int LongNounceAndCounter = 16;
     int blockSize = 16; 
     int blockSizePad;
+    int lengthBlock;
+    int offset;
     byte[] plain = new byte[DataNumber];
     byte[] keyBytes = new byte[LongKeyByte];
     byte[] nonceAndCounter = new byte[LongNounceAndCounter];
     Twofish twofish = new Twofish();
     PKCS7 padding = new PKCS7();
+    NonceCnt noncent = new NonceCnt();
 
     //Generate dummy data
     for(int i = 0; i < DataNumber; i++){
@@ -47,17 +50,39 @@ public class TestTwoFish256{
     }
     System.out.println("");
     Object keyObject = twofish.makeKey(keyBytes, blockSize);
+    
+    //Generate dummy nonce
+    int longnonce = 12;
+    byte[] nonce = new byte[longnonce];
+
+    for(byte i = 0; i < longnonce; i++){
+        nonce[i] = (byte)(rand.nextInt(256) - 128);
+    }
+    System.out.println("nonce:");
+    for(int i = longnonce - 1; i > -1; i--){
+        System.out.print(nonce[i] + " ");
+    }
+    System.out.println("");
+
+    if(plain.length % blockSize == 0){
+        lengthBlock = plain.length / 16;
+    } else {
+        lengthBlock = (plain.length / 16) + 1;
+    }
 
     //Add padding if need
+    //Teu dipake
     padding.init(blockSize);
     byte pad[] = padding.pad(plain, 0, plain.length);
     byte pt[] = new byte[plain.length + pad.length];
     System.arraycopy(plain, 0, pt, 0, plain.length);
     System.arraycopy(pad, 0, pt, plain.length, pad.length);
 
-    byte[] encrypted = new byte[pt.length];
-    for (int i=0; i<Array.getLength(pt); i+=blockSize) {
-        twofish.encrypt(pt, 0, encrypted, 0, keyObject, blockSize);
+    offset = 0;
+    byte[] encrypted = new byte[plain.length];
+    for (int i=0; i<Array.getLength(plain); i+=blockSize) {
+        nonceAndCounter = noncent.generate(offset+i,nonce);
+        twofish.encrypt(nonceAndCounter, 0, encrypted, 0, keyObject, blockSize);
     }
     
     //Print encrypted data
@@ -67,21 +92,51 @@ public class TestTwoFish256{
     }
     System.out.println("");
 
+    //Xor data
+    for(int i = 0; i < encrypted.length; i++){
+        encrypted[i] = (byte)(encrypted[i] ^ plain[i]);
+    }
+
+    //Print encrypted data
+    System.out.println("XOR Encrypted data:");
+    for(int i = 0; i < encrypted.length; i++){
+        System.out.print(encrypted[i] + " ");
+    }
+    System.out.println("");
+
     Object keydecObject = twofish.makeKey(keyBytes, 16);
     byte[] decrypted = new byte[encrypted.length];
-
-    for (int i=0; i<Array.getLength(encrypted); i+=16) {
-        twofish.decrypt(encrypted, i, decrypted, i, keydecObject, 16);
-    }
     
+    offset = 0;
+    for (int i=0; i<Array.getLength(encrypted); i+=16) {
+        nonceAndCounter = noncent.generate(offset+i,nonce);
+        twofish.encrypt(nonceAndCounter, i, decrypted, i, keydecObject, 16);
+    }
+
+    //Unpad ngga dipake juga
+    /*
     int unpad = padding.unpad(decrypted, 0, decrypted.length);
     byte[] output = new byte[decrypted.length - unpad];
-
+    
     System.arraycopy(decrypted, 0, output, 0, output.length);
+    */
+
     //Print decrypted data
     System.out.println("Decrypted data:");
-    for(int i = 0; i < output.length; i++){
-        System.out.print(output[i] + " ");
+    for(int i = 0; i < decrypted.length; i++){
+        System.out.print(decrypted[i] + " ");
+    }
+    System.out.println("");
+
+    //Xor data
+    for(int i = 0; i < encrypted.length; i++){
+        decrypted[i] = (byte)(encrypted[i] ^ decrypted[i]);
+    }
+    
+    //Print decrypted data
+    System.out.println("XOR Decrypted data:");
+    for(int i = 0; i < decrypted.length; i++){
+        System.out.print(decrypted[i] + " ");
     }
     System.out.println("");
     }
